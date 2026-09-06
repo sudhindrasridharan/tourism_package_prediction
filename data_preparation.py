6e35e7ce80e0fbc192328d27b45f1f7071500225
sudhindrasridharan <47617645+sudhindrasridharan@users.noreply.github.com>
2026-09-06 17:06:43 +0530

    Update data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -16,7 +16,7 @@ BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_PATH = os.path.join(
     BASE_PATH,
     "data",
-    "Tourism.csv"
+    "tourism.csv"
 )
 
 PROCESSED_PATH = os.path.join(
